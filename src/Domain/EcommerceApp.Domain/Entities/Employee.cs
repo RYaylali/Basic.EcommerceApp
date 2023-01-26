c54c4744090caa158989701f885204c0375667fb
Ramazan YAYLALI <ryaylali97@gmail.com>
2023-01-26 15:41:07 +0300

    SonHal

--- src/Domain/EcommerceApp.Domain/Entities/Employee.cs
+++ src/Domain/EcommerceApp.Domain/Entities/Employee.cs
@@ -1,9 +1,13 @@
 ﻿using EcommerceApp.Domain.Enums;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+
 
 namespace EcommerceApp.Domain.Entities
 {
@@ -17,24 +21,25 @@ namespace EcommerceApp.Domain.Entities
         public DateTime? UpdateDate { get; set; }
         public DateTime BirthDate { get; set; }
         public Status Status { get; set; }
-        Roles Roles { get; set; }
-
+        public Roles Roles { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public string? ImagePath { get; set; }
+        [NotMapped]
+        public IFormFile UploadPath { get; set; }
         //Navigation Property
         public Guid? MallId { get; set; }
         public Mall? Mall { get; set; }
-
-
         //Navigation Property For Managers
         public Guid? ManagerId { get; set; }
         public Employee? Manager { get; set; }
-
-
-        public List<Employee> Employees  { get; set; }
-
+        public List<Employee> Employees { get; set; }
         public Employee()
         {
             Employees = new List<Employee>();
         }
+
+
 
     }
 }
--- src/Infrastructure/EcommerceApp.Infrastructure/Context/ECommerceAppDbContext.cs
+++ src/Infrastructure/EcommerceApp.Infrastructure/Context/ECommerceAppDbContext.cs
@@ -26,5 +26,7 @@ namespace EcommerceApp.Infrastructure.Context
 
         public DbSet<Mall> Malls { get; set; }
         public DbSet<Employee> Employees { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
